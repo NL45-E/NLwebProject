d6db76bac8042ecb74a2b5fba482f444794ff676
NL45-E <nabilsangmaestro@gmail.com>
2026-04-01 16:44:04 +0700

    change file name

--- randBLocks.js
+++ randBLocks.js
@@ -3,6 +3,7 @@ function randomBlocks() {
     const idBlocks = ["b1","b2","b3","b4","b5","b6","b7","b8","b9"];
     let rand = Math.floor(Math.random() * idBlocks.length);//randomize
     let selectedBlock = document.getElementById(idBlocks[rand]);
+    //change
     selectedBlock.style.backgroundColor = "rgba(160, 31, 57, 0.37)";
     selectedBlock.style.color = "#0b2a2a";
     selectedBlock.style.padding = "6px"
