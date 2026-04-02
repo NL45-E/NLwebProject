3b467cde550a62a6a99f8db9a2f1f76fc2e642e1
NL45-E <nabilsangmaestro@gmail.com>
2026-04-02 17:24:09 +0700

    add kelap kelip

--- blockScript.js
+++ blockScript.js
@@ -1,15 +1,21 @@
-function randomBlocks() {
+function randomBlocks(delay, className) {
     // declaration
     const idBlocks = ["b1","b2","b3","b4","b5","b6","b7","b8","b9"];
     let rand = Math.floor(Math.random() * idBlocks.length);//randomize
     let selectedBlock = document.getElementById(idBlocks[rand]);
     //change
-    selectedBlock.style.backgroundColor = "rgba(160, 31, 57, 0.37)";
-    selectedBlock.style.color = "#0b2a2a";
-    selectedBlock.style.padding = "6px"
+        selectedBlock.classList.add(`${className}`);
     setTimeout(() => {
-        selectedBlock.style.backgroundColor = "rgba(127, 255, 212, 0.334)";
-        selectedBlock.style.color = "rgb(49, 47, 47)";
-        selectedBlock.style.padding = "3px"
-    }, 1500)
+        //reset style
+        selectedBlock.classList.remove(`${className}`);
+    }, delay)
+}
+function strobo() {
+    for (let i = 0; i <= 18; i++) {
+        setTimeout(() => {
+           if (i !== 18) {
+             randomBlocks(700, 'selected');
+           } else {randomBlocks(2500, 'red')}
+        }, i * 300)
+    }
 }
